ae672d1afcdd39ef4054bb6c6af31e62de567f5c
ayacatt <k.saiko0224@gmail.com>
2021-10-02 10:03:34 +0000

    Initial Coomit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+
+end
